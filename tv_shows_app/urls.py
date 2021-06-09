57758ceb54e55233401c55676828e6ceb49988c0
Mei Manzo <mmanzo806@gmail.com>
2021-06-08 23:06:14 -0700

    added POST and GET

--- tv_shows_app/urls.py
+++ tv_shows_app/urls.py
@@ -3,7 +3,8 @@ from . import views
 urlpatterns = [
     path('shows/new', views.add),
     path('shows', views.all),
-    path('shows/2/edit', views.edit), #need to add param for '2'
-    path('shows/<id>', views.show), #need to add param for '2'
+    path('shows/<int:id>/edit', views.edit),
+    path('shows/<int:id>', views.show), 
     path('shows/create', views.create),
+    path('shows/edit_this', views.edit_this),
 ]
--- tv_shows_app/views.py
+++ tv_shows_app/views.py
@@ -7,13 +7,20 @@ def add(request):
 def edit(request):
     return render(request, "edit_show.html")
 
+def edit_this(request): #similar to def create, we are performing an action then redirecting
+    if request.method == 'POST':
+        return redirect(f"/shows/{new_show.id}/edit")
+    elif request.method == 'GET':
+        return redirect('/shows/3')
+    return redirect('/shows/new')
+
 def show(request, id): #has id in url, add id param
     context = {
-        "this_show": Shows.objects.get(id=1)
+        "this_show": Shows.objects.get(id=id)
     }
     return render(request, "tv_show.html", context)
 
-def all(request):
+def all(request): #used for our "all" display page
     context = {
         "all_shows": Shows.objects.all()
     }
@@ -22,9 +29,8 @@ def all(request):
 
 def create(request):
     if request.method=='POST':
-        Shows.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release-date'], description=request.POST['description'])
-        return redirect('/shows/'+id)
-    context = {
-        "this_show": Shows.objects.get(id=request.POST['id'])
-    }
-    return redirect('/shows/'+id, context)
+        new_show = Shows.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release-date'], description=request.POST['description'])
+        print(new_show.id)
+        return redirect(f"/shows/{new_show.id}") #just needed to convert this to a f string to get it to read
+        #redirects to def show
+    return redirect('/shows/new')
