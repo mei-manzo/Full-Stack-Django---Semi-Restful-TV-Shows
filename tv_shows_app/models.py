225f2d2e65a3e913a31bdbe037d348d9176cf29a
Mei Manzo <mmanzo806@gmail.com>
2021-06-12 21:33:07 -0700

    Added validation

--- tv_shows_app/models.py
+++ tv_shows_app/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class ShowsManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 2:
+            errors["title"] = "TV Show title should be at least 2 characters"
+        if len(postData['description']) < 10:
+            errors["description"] = "TV Show description should be at least 10 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "TV Show description should be at least 3 characters"
+        return errors
+
 class Shows(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -7,9 +19,9 @@ class Shows(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     network = models.CharField(max_length=45, default = "none")
+    objects = ShowsManager()
 
-# class Networks(models.Model):
-#     name = models.CharField(max_length=45)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+
+
+
 
--- tv_shows_app/views.py
+++ tv_shows_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from .models import *
 
 def index(request):
@@ -14,14 +15,23 @@ def edit(request, id):
     return render(request, 'edit_show.html', context)
 
 def update(request, id):
-    if request.method=='POST':
+    errors = Shows.objects.basic_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/shows/{id}/edit")
+    else:
+        # if request.method=='POST':
         updated_show = Shows.objects.get(id=id)
         updated_show.title=request.POST['title']
         updated_show.network=request.POST['network']
         updated_show.release_date=request.POST['release-date']
         updated_show.description=request.POST['description']
         updated_show.save()
-    return redirect('/shows') 
+        messages.success(request, "TV Show successfully updated")
+        return redirect(f"/shows/{id}")
+    # return redirect('/shows') 
 
 def show(request, id): #has id in url, add id param
     context = {
@@ -37,12 +47,16 @@ def all(request): #used for our "all" display page
 
 
 def create(request):
-    if request.method=='POST':
+    errors = Shows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+    # if request.method=='POST':
         new_show = Shows.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release-date'], description=request.POST['description'])
-        print(new_show.id)
         return redirect(f"/shows/{new_show.id}") #just needed to convert this to a f string to get it to read
-        #redirects to def show
-    return redirect('/shows/new')
+    # return redirect('/shows/new')
 
 def delete(request, id):
     delete_show = Shows.objects.get(id=id)
